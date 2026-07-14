fc8b08deb3df803a30e92e2454f5dd7e9df49890
Devlusket <tsuunakage@gmail.com>
2026-07-13 21:45:58 -0300

    Aplica JWT nas operacoes do Swagger

--- src/main/java/com/financa/config/OpenApiConfig.java
+++ src/main/java/com/financa/config/OpenApiConfig.java
@@ -3,6 +3,7 @@ package com.financa.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,7 @@ public class OpenApiConfig {
                         .title("Finança API")
                         .version("v1")
                         .description("API de controle financeiro doméstico por casa."))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
                 .components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme()
                         .type(SecurityScheme.Type.HTTP)
                         .scheme("bearer")
